37912a06b6da48e273c77d398139a980eb3b9610
Marc Schuricht <marcs@slalom.com>
2015-03-09 15:05:39 -0600

    binding

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,6 +12,13 @@ angular
     'ui.bootstrap'
   ])
   .controller('mainCtrl', ['$scope', function ($scope) {
-    
+    $scope.newTask = {};
+    $scope.tasks = [];
+    $scope.saveTask = function (ev) {
+      if(ev.keyCode === 13 && $scope.addItem.$valid){
+        $scope.tasks.push($scope.newTask);
+        $scope.newTask = {};
+      }
+    }
   }]);
 
